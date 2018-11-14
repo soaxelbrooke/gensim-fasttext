2ef1c3ceb5ae47fc9ca8bb6e9469542d8cc301ad
Stuart Axelbrooke <stuart@axelbrooke.com>
2018-11-14 20:59:31 +0900

    Add more parameters to env, save them in sqlite db

--- train.py
+++ train.py
@@ -15,6 +15,31 @@ VOCAB_SIZE = int(os.getenv("VOCAB_SIZE", 100_000))
 EMBED_DIM = int(os.getenv("EMBED_DIM", 300))
 LIMIT = int(os.getenv("LIMIT", 1_000_000_000))
 EPOCHS = int(os.getenv("EPOCHS", 1))
+SKIPGRAM = int(os.getenv("SKIPGRAM", 1))
+WINDOW_LEN = int(os.getenv("WINDOW_LEN", 5))
+MIN_COUNT = int(os.getenv("MIN_COUNT", 20))
+LEARNING_RATE = float(os.getenv("LEARNING_RATE", 0.1))
+MIN_LEARNING_RATE = float(os.getenv("MIN_LEARNING_RATE", 0.0001))
+NUM_WORKERS = int(os.getenv("NUM_WORKERS", 8))
+NEGATIVE_SAMPLES = int(os.getenv("NEGATIVE_SAMPLES", 10))
+
+
+def jsonify_build_params() -> str:
+    return json.dumps(
+        {
+            "REGEX_MATCH_PATTERN": REGEX_MATCH_PATTERN,
+            "VOCAB_SIZE": VOCAB_SIZE,
+            "EMBED_DIM": EMBED_DIM,
+            "LIMIT": LIMIT,
+            "EPOCHS": EPOCHS,
+            "SKIPGRAM": SKIPGRAM,
+            "WINDOW_LEN": WINDOW_LEN,
+            "MIN_COUNT": MIN_COUNT,
+            "LEARNING_RATE": LEARNING_RATE,
+            "MIN_LEARNING_RATE": MIN_LEARNING_RATE,
+            "NUM_WORKERS": NUM_WORKERS,
+        }
+    )
 
 
 def main(inpath: str):
@@ -29,38 +54,44 @@ def main(inpath: str):
             corpus.append(doc)
             for token in doc:
                 token_counts[token] += 1
-    
-    if os.path.exists('vectors_out.sqlite'):
-        os.unlink('vectors_out.sqlite')
 
-    conn = sqlite3.connect('vectors_out.sqlite')
-    conn.execute('''
+    if os.path.exists("vectors_out.sqlite"):
+        os.unlink("vectors_out.sqlite")
+
+    conn = sqlite3.connect("vectors_out.sqlite")
+    conn.execute(
+        """
         CREATE TABLE vector_meta (
             vector_float_bytes integer, 
             embedding_dimensions integer,
-            vocab_size integer
+            vocab_size integer,
+            build_parameters text
         )
-    ''')
-    conn.execute('INSERT INTO vector_meta VALUES (?, ?, ?)',
-                 ('float32', EMBED_DIM, VOCAB_SIZE))
-    conn.execute('CREATE TABLE vectors (token text primary key, vector_bytes blob);')
-    conn.execute('CREATE TABLE frequencies (token text primary key, count integer);')
+    """
+    )
+    conn.execute(
+        "INSERT INTO vector_meta VALUES (?, ?, ?, ?)",
+        ("float32", EMBED_DIM, VOCAB_SIZE, jsonify_build_params()),
+    )
+    conn.execute("CREATE TABLE vectors (token text primary key, vector_bytes blob);")
+    conn.execute("CREATE TABLE frequencies (token text primary key, count integer);")
 
     print(f"Loaded {sum(token_counts.values())} words.  Training model...")
 
     ft_model = FastText(
-        corpus, 
-        size=EMBED_DIM, 
-        window=5, 
-        min_count=10, 
+        corpus,
+        size=EMBED_DIM,
+        window=WINDOW_LEN,
+        min_count=MIN_COUNT,
         max_vocab_size=VOCAB_SIZE,
-        alpha=0.1,
-        sg=1,
+        alpha=LEARNING_RATE,
+        sg=SKIPGRAM,
         sorted_vocab=1,
         iter=EPOCHS,
-        workers=8,
+        workers=NUM_WORKERS,
+        negative=NEGATIVE_SAMPLES,
     )
-    
+
     vectors = {}
     vocab = {}
     sorted_counts = sorted(token_counts.items(), key=lambda p: -p[1])
@@ -74,9 +105,9 @@ def main(inpath: str):
             continue
         vectors[token] = vector
         vocab[token] = idx
-        vector_bytes = vector.astype('float32').tobytes()
-        conn.execute('INSERT INTO vectors VALUES (?, ?)', (token, vector_bytes))
-        conn.execute('INSERT INTO frequencies VALUES (?, ?)', (token, count))
+        vector_bytes = vector.astype("float32").tobytes()
+        conn.execute("INSERT INTO vectors VALUES (?, ?)", (token, vector_bytes))
+        conn.execute("INSERT INTO frequencies VALUES (?, ?)", (token, count))
         progress.update(1)
         idx += 1
         if idx >= VOCAB_SIZE:
@@ -84,15 +115,16 @@ def main(inpath: str):
 
     progress.close()
     print("Writing vocab.json...")
-    with open('vocab.json', 'w') as outfile:
+    with open("vocab.json", "w") as outfile:
         json.dump(vocab, outfile)
     print("Writing vectors_out.sqlite...")
     conn.commit()
     print("Writing embeddings_out.txt...")
-    pandas.DataFrame.from_dict(vectors, orient='index') \
-        .to_csv('embeddings_out.txt', header=None, sep=" ", quoting=csv.QUOTE_NONE)
+    pandas.DataFrame.from_dict(vectors, orient="index").to_csv(
+        "embeddings_out.txt", header=None, sep=" ", quoting=csv.QUOTE_NONE
+    )
     print("Done!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1])
