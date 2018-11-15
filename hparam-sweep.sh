2b79e6fe5c2b938ca775cc98b86284b1b7c4eac0
Stuart Axelbrooke <stuart@axelbrooke.com>
2018-11-15 16:16:35 +0900

    Add grid search script

---
+++ hparam-sweep.sh
@@ -0,0 +1,13 @@
+
+mkdir -p out
+for EPOCHS in 5 50 100; do
+    for WINDOW_LEN in 3 5 7; do
+        for NEG_SAMP_DIST in -0.5 0.75; do
+            export OUTDIR="out/e${EPOCHS}_wl${WINDOW_LEN}_nsd${NEG_SAMP_DIST}"
+            echo "Training vectors with epochs=$EPOCHS, window length=$WINDOW_LEN, neg sampling exponent=$NEG_SAMP_DIST..."
+            python3 train.py train
+            mv vectors_out.sqlite out/$OUTDIR/vectors.sqlite
+        done
+    done
+done
+
