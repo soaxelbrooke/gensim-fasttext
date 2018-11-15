52c1d2a4803914ef16a0d40bab7eb8d10d79b6dc
Stuart Axelbrooke <stuart@axelbrooke.com>
2018-11-15 16:05:40 +0900

    Update to build then train strategy to save memory

--- train.py
+++ train.py
@@ -1,14 +1,18 @@
 
+import subprocess
+from nltk import sent_tokenize
 import os
 import re
 import sys
 from tqdm import tqdm
-from typing import Counter
+from typing import Counter, List, Tuple
 from gensim.models import FastText
 import pandas
+import toolz
 import sqlite3
 import csv
 import json
+from multiprocessing import Pool
 
 REGEX_MATCH_PATTERN = os.getenv("REGEX_MATCH_PATTERN", r"[\w']+|[,\.\?!;\-\(\)]")
 VOCAB_SIZE = int(os.getenv("VOCAB_SIZE", 100_000))
@@ -42,18 +46,81 @@ def jsonify_build_params() -> str:
     )
 
 
-def main(inpath: str):
+def calculate_corpus(lines: List[str]) -> Tuple[str, Counter]:
     regex = re.compile(REGEX_MATCH_PATTERN)
     token_counts = Counter()
-    corpus = []
-    with open(inpath) as infile:
-        for idx, line in enumerate(tqdm(infile, desc="Loading corpus")):
-            if idx >= LIMIT:
-                break
+    result_lines = []
+    for line in lines:
+        for sentence in sent_tokenize(line):
             doc = regex.findall(line)
-            corpus.append(doc)
             for token in doc:
                 token_counts[token] += 1
+            result_lines.append(" ".join(doc))
+    return "\n".join(result_lines), token_counts
+
+
+def build_corpus(inpath: str):
+    print(f"Building corpus from {inpath}...")
+    total_lines = int(subprocess.check_output(["wc", "-l", inpath]).split(b" ")[0])
+    total_lines = min([total_lines, LIMIT])
+    num_sentences = 0
+    regex = re.compile(REGEX_MATCH_PATTERN)
+    token_counts = Counter()
+    with open(inpath) as infile:
+        with open("corpus.txt", "w") as outfile:
+            for idx, line in enumerate(tqdm(infile, desc="Building corpus", total=total_lines)):
+                if idx >= LIMIT:
+                    break
+                for sentence in sent_tokenize(line):
+                    num_sentences += 1
+                    doc = regex.findall(sentence)
+                    for token in doc:
+                        token_counts[token] += 1
+                    outfile.write(" ".join(doc) + "\n")
+
+    print(
+        f"Processed {sum(token_counts.values())} words in {num_sentences} sentences from {total_lines} lines."
+    )
+    with open("token_counts.json", "w") as outfile:
+        json.dump(dict(token_counts), outfile)
+    print(
+        "Wrote corpus to ./corpus.txt in line format, ' '-separated tokens, and wrote token counts to token_counts.json."
+    )
+
+
+def main():
+    try:
+        with open("token_counts.json") as infile:
+            token_counts = json.load(infile)
+    except:
+        print("Failed to load token counts - did you `python train.py build-corpus $INFILE` yet?")
+        sys.exit(1)
+
+    # corpus = []
+    # with open(inpath) as infile:
+    #     for idx, line in enumerate(tqdm(infile, desc="Loading corpus", total=total_lines)):
+    #         if idx >= LIMIT:
+    #             break
+    #         for sentence in sent_tokenize(line):
+    #             doc = regex.findall(sentence)
+    #             corpus.append(doc)
+    #             for token in doc:
+    #                 token_counts[token] += 1
+
+    # total_lines = int(subprocess.check_output(['wc', '-l', inpath]).split(b' ')[0])
+    # corpus = []
+    # token_counts = Counter()
+    # # pool = Pool(NUM_WORKERS)
+    # with open(inpath) as infile:
+    #     lines = (line.strip() for line in infile)
+    #     batches = toolz.partition_all(10000, lines)
+    #     total_batches = int(total_lines / 10000) + 1
+    #     for batch_corpus, batch_token_counts in tqdm(map(calculate_corpus, batches), total=total_batches):
+    #         # print(f"Processing batch corpus of {len(batch_corpus)} sentences and {sum(batch_token_counts.values())} words.")
+    #         with open('corpus.tmp', 'a') as corpusfile:
+    #             corpusfile.write(batch_corpus)
+    #         token_counts += batch_token_counts
+    #         # print(f"Counted {len(token_counts)} unique tokens so far...")
 
     if os.path.exists("vectors_out.sqlite"):
         os.unlink("vectors_out.sqlite")
@@ -79,7 +146,7 @@ def main(inpath: str):
     print(f"Loaded {sum(token_counts.values())} words.  Training model...")
 
     ft_model = FastText(
-        corpus,
+        corpus_file="corpus.txt",
         size=EMBED_DIM,
         window=WINDOW_LEN,
         min_count=MIN_COUNT,
@@ -127,4 +194,7 @@ def main(inpath: str):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    if sys.argv[1] == "train":
+        main()
+    elif sys.argv[1] == "build-corpus":
+        build_corpus(sys.argv[2])
